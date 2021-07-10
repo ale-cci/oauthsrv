efd95d83d970b0faa3b35c43bfd3f82ef0b63702
ale-cci <alecrd98@gmail.com>
2021-07-10 17:28:41 +0200

    refactor: cleanup jwt parsing logic

--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -1,6 +1,7 @@
 package jwt
 
 import (
+	"bytes"
 	"crypto/rsa"
 	"encoding/base64"
 	"encoding/json"
@@ -13,15 +14,19 @@ type JWT struct {
 	Body JWTBody
 }
 type JWTHead struct {
-	Alg string `json:"alg"`
+	Alg string
+	Typ string
 }
 
-type JWTBody map[string]string
+type JWTBody map[string]interface{}
 
 func (j *JWT) Encode(pk *rsa.PrivateKey) string {
 	bytes, _ := json.Marshal(j.Body)
 	body := base64.RawURLEncoding.EncodeToString(bytes)
-	return "." + body + "."
+
+	headBytes, _ := json.Marshal(j.Head)
+	head := base64.RawURLEncoding.EncodeToString(headBytes)
+	return head + "." + body + "."
 }
 
 func New(body JWTBody) *JWT {
@@ -30,21 +35,35 @@ func New(body JWTBody) *JWT {
 	}
 }
 
+func parseChunk(chunk string, into interface{}) error {
+	decodedChunk, err := base64.RawURLEncoding.DecodeString(chunk)
+
+	if err != nil {
+		return err
+	}
+
+	decoder := json.NewDecoder(bytes.NewReader(decodedChunk))
+	decoder.UseNumber()
+
+	return decoder.Decode(into)
+}
+
 func Decode(token string) (*JWT, error) {
 	chunks := strings.Split(token, ".")
 	if amount := len(chunks); amount != 3 {
 		return nil, fmt.Errorf("Wrong number of chunks, want 3, got %d", amount)
 	}
-	head, body := chunks[0], chunks[1]
-
-	headDecoded, _ := base64.RawURLEncoding.DecodeString(head)
-	bodyDecoded, _ := base64.RawURLEncoding.DecodeString(body)
 
 	var jwtHead JWTHead
 	var jwtBody JWTBody
 
-	json.Unmarshal(headDecoded, &jwtHead)
-	json.Unmarshal(bodyDecoded, &jwtBody)
+	if err := parseChunk(chunks[0], &jwtHead); err != nil {
+		return nil, err
+	}
+
+	if err := parseChunk(chunks[1], &jwtBody); err != nil {
+		return nil, err
+	}
 
 	return &JWT{&jwtHead, jwtBody}, nil
 }
--- pkg/jwt/jwt_test.go
+++ pkg/jwt/jwt_test.go
@@ -1,6 +1,7 @@
 package jwt_test
 
 import (
+	"fmt"
 	"github.com/ale-cci/oauthsrv/pkg/jwt"
 	"strings"
 	"testing"
@@ -18,7 +19,7 @@ func TestJWT(t *testing.T) {
 		}
 	})
 
-	t.Run("Same field should return back if token is decoded", func(t *testing.T) {
+	t.Run("encode + decode should return same jwt-body fields", func(t *testing.T) {
 		token := jwt.New(jwt.JWTBody{
 			"iss": "myself",
 		}).Encode(nil)
@@ -39,37 +40,68 @@ func TestJWT(t *testing.T) {
 func TestDecode(t *testing.T) {
 	exampleToken := "eyJhbGciOiJub25lIn0.eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ."
 
-	// example from https://datatracker.ietf.org/doc/html/rfc7519#appendix-A.1
-	t.Run("should parse head algorithm correctly", func(t *testing.T) {
-		token, err := jwt.Decode(exampleToken)
+	exampleTokens := []struct {
+		Token string
+		Head  *jwt.JWTHead
+		Body  jwt.JWTBody
+	}{
+		{
+			Token: "eyJhbGciOiJub25lIn0.eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ.",
+			Head:  &jwt.JWTHead{Alg: "none"},
+			Body: jwt.JWTBody{
+				"iss":                        "joe",
+				"exp":                        1300819380,
+				"http://example.com/is_root": true,
+			},
+		},
+		{
+			Token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
+			Head:  &jwt.JWTHead{Alg: "HS256", Typ: "JWT"},
+			Body: jwt.JWTBody{
+				"sub":  1234567890,
+				"name": "John Doe",
+				"iat":  1516239022,
+			},
+		},
+	}
 
-		if err != nil {
-			t.Errorf("Unexpected error: %v", err)
-		}
+	t.Run("should read alg from jwt-head", func(t *testing.T) {
+		for i, token := range exampleTokens {
+			t.Run(fmt.Sprintf("Suite %d", i), func(t *testing.T) {
+				decoded, err := jwt.Decode(token.Token)
 
-		got := token.Head.Alg
-		want := "none"
-		if got != want {
-			t.Errorf("want: %q, got %q", want, got)
+				if err != nil {
+					t.Errorf("Unexpected error: %v", err)
+				}
+
+				got := fmt.Sprint(decoded.Head)
+				want := fmt.Sprint(token.Head)
+				if got != want {
+					t.Errorf("want: %q, got %q", want, got)
+				}
+			})
 		}
 	})
 
-	t.Run("should correctly get three fields from body", func(t *testing.T) {
-		token, err := jwt.Decode(exampleToken)
+	t.Run("should read fields from jwt-body", func(t *testing.T) {
+		for i, token := range exampleTokens {
+			t.Run(fmt.Sprintf("Suite %d", i), func(t *testing.T) {
+				decoded, err := jwt.Decode(token.Token)
 
-		if err != nil {
-			t.Errorf("Unexpected error: %v", err)
-		}
+				if err != nil {
+					t.Errorf("Unexpected error: %v", err)
+				}
+				want := fmt.Sprint(token.Body)
+				got := fmt.Sprint(decoded.Body)
 
-		got := token.Body["iss"]
-		want := "joe"
-
-		if got != want {
-			t.Errorf("want: %q, got %q", want, got)
+				if want != got {
+					t.Errorf("want: %q, got: %q", want, got)
+				}
+			})
 		}
 	})
 
-	t.Run("should return error if chunks are higher than three", func(t *testing.T) {
+	t.Run("should return error if token contains more than 3 chunks", func(t *testing.T) {
 		_, err := jwt.Decode(exampleToken + ".")
 
 		if err == nil {
@@ -77,7 +109,7 @@ func TestDecode(t *testing.T) {
 		}
 	})
 
-	t.Run("should return error if chunks are less than three", func(t *testing.T) {
+	t.Run("should return error if token contains less than three chunks", func(t *testing.T) {
 		_, err := jwt.Decode(".")
 
 		if err == nil {
