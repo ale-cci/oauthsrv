3abfcacf40076504c3c30e2a669ca907feedae33
ale-cci <alecrd98@gmail.com>
2021-07-10 11:00:48 +0200

    chore: skip test if no go files are committed

--- git-hooks/pre-commit.d/run-tests.sh
+++ git-hooks/pre-commit.d/run-tests.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 set -e -o pipefail
-echo $@
+
+go_files=`git diff --cached --name-only HEAD | { grep -e '\.go$' || true; }`
+
+[[ -z "$go_files" ]] && echo "Skip" && exit 0
 docker-compose run --rm oauthsrv go test -timeout 30s ./...
