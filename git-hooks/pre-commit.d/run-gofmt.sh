943c1d5823e97c18e59753e8d623a7277356647e
ale-cci <alecrd98@gmail.com>
2021-07-11 19:45:48 +0200

    feat: block commit if files have to be re-added cause not formatted

--- git-hooks/pre-commit.d/run-gofmt.sh
+++ git-hooks/pre-commit.d/run-gofmt.sh
@@ -7,4 +7,7 @@ go_files=`git diff --cached --name-only --diff-filter=d HEAD | { grep -e '\.go$'
 
 
 echo "Formatting files..."
-gofmt -l -w $go_files
+
+exec 5>&1
+output=`gofmt -l -w $go_files | tee /dev/fd/5`
+[[ -z "$output" ]]
