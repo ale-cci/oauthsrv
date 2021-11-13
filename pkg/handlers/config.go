fb83485d7306fab2b92964357f0d4fda956806b8
ale-cci <alecrd98@gmail.com>
2021-11-13 16:55:38 +0100

    refactor: add documentation to config.go

--- pkg/handlers/config.go
+++ pkg/handlers/config.go
@@ -1,7 +1,11 @@
+/**
+ * Package to manage application configuration/settings
+ */
 package handlers
 
 import (
 	"context"
+	"crypto/rsa"
 	"fmt"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -9,11 +13,33 @@ import (
 	"os"
 )
 
-type Config struct {
-	Database *mongo.Database
+/**
+ * Private/Public keystore interface
+ * It's usage is to retrieve a public and private keys from a generic source,
+ * could be from memory or from a database.
+ */
+type PKStore interface {
+	GetPublicKey(alg string, kid string) (*rsa.PublicKey, error)
+	GetPrivateKey(alg string, kid string) (*rsa.PrivateKey, error)
+
+	// list keyid algorithm
+	ListKeys() map[string]string
 }
 
-// Read service configuration from environment variables
+/**
+ * Application configuration, injected to each handlers registered.
+ */
+type Config struct {
+	// open connection to the mongo database
+	Database *mongo.Database
+
+	// application keystore, used to sign jwts
+	KeyStore PKStore
+}
+
+/**
+ * Generate `Config` data structure using environment variables
+ */
 func EnvConfig() (*Config, error) {
 	mongoConnStr := os.Getenv("MONGO_CONNSTR")
 	client, err := mongo.NewClient(options.Client().ApplyURI(mongoConnStr))
@@ -30,6 +56,10 @@ func EnvConfig() (*Config, error) {
 	}, nil
 }
 
+/**
+ * Inject the configuration to a custom handler function,
+ * returning a standard `http.HandlerFunc`
+ */
 func (cnf *Config) apply(handler func(*Config, http.ResponseWriter, *http.Request)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		handler(cnf, w, r)
