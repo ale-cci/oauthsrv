cba6d63c8463880e6a1cd0bb9d7a8fe3fbf48b3d
ale-cci <alecrd98@gmail.com>
2021-11-13 17:15:01 +0100

    feat(OIDC-2): add client-credentials example

---
+++ examples/client_credentials.py
@@ -0,0 +1,25 @@
+'''
+Example on how to retrieve a jwt from machine to machine point of view.
+Your application should be registered on the oauthsrv server in order to work.
+In the registration process you should receive a `client_id` and a `client_secret`.
+
+NOTE: this feature is not implemented in `oauthsrv`
+'''
+import requests
+
+client_id = '<MY-CLIENT-ID>'
+client_secret = '<MY-CLIENT-PASSWORD>'
+
+
+def main():
+    resp = requests.post('http://localhost:8080/oauth/v2/auth?grant_type=client_credentials', data={
+        'client_id': client_id,
+        'client_secret': client_secret
+    })
+
+    assert resp.status_code == 200, f'{resp.status_code} != 200'
+    assert 'access_token' in resp.json()
+
+
+if __name__ == '__main__':
+   main()
