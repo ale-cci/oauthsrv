b143d616755b2b249ae1e48c212022f32f7d30c2
ale-cci <alecrd98@gmail.com>
2021-11-15 21:55:26 +0100

    feat(OIDC-15): add basic method of grant_type password

--- pkg/handlers/handle_grant_password.go
+++ pkg/handlers/handle_grant_password.go
@@ -3,5 +3,8 @@ package handlers
 import "net/http"
 
 func handleGrantPassword(cnf *Config, w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
 	http.Error(w, "Nope", http.StatusUnauthorized)
 }
--- pkg/handlers/handle_grant_password_test.go
+++ pkg/handlers/handle_grant_password_test.go
@@ -5,6 +5,7 @@ import (
 	"gotest.tools/assert"
 	"log"
 	"net/http"
+	"net/url"
 	"testing"
 )
 
@@ -16,13 +17,25 @@ func TestHandleAuthPassword(t *testing.T) {
 	client := NoFollowRedirectClient(srv)
 	requestPath := "/oauth/v2/auth?grant_type=password"
 
-	t.Run("should return 401 if user is not registered", func(t *testing.T) {
+	t.Run("only post request should be allowed", func(t *testing.T) {
 		resp, err := client.Get(srv.URL + requestPath)
 		if err != nil {
 			log.Fatalf("Unexpected error: %v", err)
 		}
 
-		assert.Equal(t, resp.StatusCode, http.StatusUnauthorized)
+		assert.Equal(t, resp.StatusCode, http.StatusMethodNotAllowed)
 
+	})
+	t.Run("incorrect credentials should return 401", func(t *testing.T) {
+		resp, err := client.PostForm(srv.URL+requestPath, url.Values{
+			"username": {"wrong@email.com"},
+			"password": {"root"},
+		})
+
+		if err != nil {
+			log.Fatalf("Unexpected error: %v", err)
+		}
+
+		assert.Equal(t, resp.StatusCode, http.StatusUnauthorized)
 	})
 }
