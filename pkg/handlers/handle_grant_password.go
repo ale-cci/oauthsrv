7b5843c7e67b27a8afe5b1b0656a1f1deca978b9
ale-cci <alecrd98@gmail.com>
2021-11-17 23:00:42 +0100

    feat(OIDC-8): add basic method and user checking

--- pkg/handlers/handle_grant_password.go
+++ pkg/handlers/handle_grant_password.go
@@ -1,10 +1,32 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func handleGrantPassword(cnf *Config, w http.ResponseWriter, r *http.Request) {
+	enc := json.NewEncoder(w)
+
 	if r.Method != "POST" {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		enc.Encode(map[string]string{
+			"status":  "error",
+			"message": "Method not allowed",
+		})
+		return
 	}
-	http.Error(w, "Nope", http.StatusUnauthorized)
+
+	username := r.FormValue("username")
+	// password := r.FormValue("password")
+
+	if username == "test@email.com" {
+		w.WriteHeader(http.StatusOK)
+	} else {
+		w.WriteHeader(http.StatusUnauthorized)
+	}
+	enc.Encode(map[string]string{
+		"status":  "error",
+		"message": "Not Authorized",
+	})
 }
--- pkg/handlers/handle_grant_password_test.go
+++ pkg/handlers/handle_grant_password_test.go
@@ -1,12 +1,19 @@
 package handlers_test
 
 import (
-	"github.com/ale-cci/oauthsrv/pkg/handlers"
-	"gotest.tools/assert"
+	"context"
+	"crypto/rand"
+	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 	"net/url"
 	"testing"
+
+	"github.com/ale-cci/oauthsrv/pkg/handlers"
+	"github.com/ale-cci/oauthsrv/pkg/passwords"
+	"go.mongodb.org/mongo-driver/bson"
+	"gotest.tools/assert"
 )
 
 func TestHandleAuthPassword(t *testing.T) {
@@ -25,6 +32,19 @@ func TestHandleAuthPassword(t *testing.T) {
 
 		assert.Equal(t, resp.StatusCode, http.StatusMethodNotAllowed)
 
+		t.Run("response should be a valid json", func(t *testing.T) {
+			data, err := io.ReadAll(resp.Body)
+			if err != nil {
+				log.Fatalf("Unable to read response body: %v", err)
+			}
+
+			var fields map[string]string
+			err = json.Unmarshal(data, &fields)
+			if err != nil {
+				log.Fatalf("Unable to decode json body: %v", err)
+			}
+		})
+
 	})
 	t.Run("incorrect credentials should return 401", func(t *testing.T) {
 		resp, err := client.PostForm(srv.URL+requestPath, url.Values{
@@ -37,5 +57,39 @@ func TestHandleAuthPassword(t *testing.T) {
 		}
 
 		assert.Equal(t, resp.StatusCode, http.StatusUnauthorized)
+		t.Run("response should be a valid json", func(t *testing.T) {
+			data, err := io.ReadAll(resp.Body)
+			if err != nil {
+				log.Fatalf("Unable to read response body: %v", err)
+			}
+
+			var fields map[string]string
+			err = json.Unmarshal(data, &fields)
+			if err != nil {
+				log.Fatalf("Unable to decode json body: %v", err)
+			}
+		})
+	})
+
+	t.Run("correct credentials should return jwt", func(t *testing.T) {
+		// add user to db
+		password, _ := passwords.New(rand.Reader, "test")
+		cnf.Database.Collection("identities").InsertOne(
+			context.Background(),
+			bson.D{
+				{Key: "_id", Value: "test@email.com"},
+				{Key: "password", Value: password},
+			},
+		)
+
+		resp, err := client.PostForm(srv.URL+requestPath, url.Values{
+			"username": {"test@email.com"},
+			"password": {"test"},
+		})
+		if err != nil {
+			log.Fatalf("Unexpected error: %v", err)
+		}
+
+		assert.Equal(t, resp.StatusCode, http.StatusOK)
 	})
 }
